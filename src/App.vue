0c26773bbcfa6cb515ce7b9ddbf55a43eeb04d3e
liva <liv.ratsimbazafy@gmail.com>
2021-10-08 14:51:36 +0200

    adding livedatetime & dynamic tage title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-    <v-app id="inspire">   
-        <!-- Sidebar -->     
-        <v-navigation-drawer v-model="drawer" app>
-            <v-list-item>
-                <v-list-item-content>
-                    <v-list-item-title class="text-h6">Todo</v-list-item-title>
-                    <v-list-item-subtitle>reload man</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-
-            <v-divider></v-divider>
+    <v-app id="inspire">
+        <!-- Sidebar -->
+        <v-navigation-drawer v-model="drawer" app :mobile-breakpoint="768">
+            <v-img
+                src="mountains.jpg"
+                height="170"
+                gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+                class="pa-4 pt-6"
+            >
+                <v-avatar size="70" class="mb-2">
+                    <img
+                        src="https://cdn.vuetifyjs.com/images/john.jpg"
+                        alt="John"                        
+                    />
+                </v-avatar>
+                <div class="white--text text-subtitle-1 font-weight-bold">
+                    John
+                </div>
+                <div class="white--text text-subtitle-2">
+                    Doe
+                </div>
+            </v-img>
 
             <v-list dense nav>
                 <v-list-item
@@ -26,16 +37,17 @@
                         <v-list-item-title>{{ item.title }}</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
-            </v-list>
-        </v-navigation-drawer><!-- end_sidebar -->
-      
+            </v-list> </v-navigation-drawer
+        ><!-- end_sidebar -->
+
         <!-- Navbar -->
-        <v-app-bar 
-            app           
+        <v-app-bar
+            app
             color="primary"
-            dark            
-            src="mountains.jpg" 
-            prominent           
+            dark
+            src="mountains.jpg"
+            prominent
+            height="170"
         >
             <template v-slot:img="{ props }">
                 <v-img
@@ -44,30 +56,34 @@
                 ></v-img>
             </template>
 
-            <v-container class="pa-0">
+            <v-container class="header-container pa-0">
                 <v-row>
-                    <v-app-bar-nav-icon @click="drawer = !drawer" class="mt-2"></v-app-bar-nav-icon>
+                    <v-app-bar-nav-icon
+                        @click="drawer = !drawer"
+                        class="mt-2"
+                    ></v-app-bar-nav-icon>
                     <v-spacer></v-spacer>
                     <search-bar />
                 </v-row>
                 <v-row>
-                    <v-app-bar-title class="ml-4">Vuetify-Todo</v-app-bar-title>                    
+                    <v-toolbar-title class="text-h4 font-weight-bold ml-4">VTasks</v-toolbar-title>
                 </v-row>
-            </v-container>
-                        
-        </v-app-bar><!-- End_Navbar -->
 
-        <v-main>            
+                <live-date />
+            </v-container> </v-app-bar
+        ><!-- End_Navbar -->
+
+        <v-main>
             <router-view></router-view>
             <snackbar />
         </v-main>
-
     </v-app>
 </template>
 
 <script>
-import Snackbar from './components/Shared/Snackbar.vue';
-import SearchBarVue from './components/Tools/SearchBar.vue';
+import Snackbar from "./components/Shared/Snackbar.vue";
+import LiveDateVue from "./components/Tools/LiveDate.vue";
+import SearchBarVue from "./components/Tools/SearchBar.vue";
 
 export default {
     data: () => ({
@@ -76,11 +92,17 @@ export default {
             { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
             { title: "About", icon: "mdi-help-box", to: "/about" },
         ],
-        
     }),
     components: {
-        'snackbar': Snackbar,
-        'search-bar': SearchBarVue
-    }
+        snackbar: Snackbar,
+        "search-bar": SearchBarVue,
+        "live-date": LiveDateVue,
+    },
 };
 </script>
+
+<style scoped>
+    .header-container {
+        max-width: none;
+    }   
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -24,4 +24,9 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+    document.title = `VTasks - ${to.name}`;
+    next();
+})
+
 export default router
